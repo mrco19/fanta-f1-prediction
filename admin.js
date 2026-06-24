ec2bc077c61af2d4f516949c381f6675c6630baa
mrco19 <mrco360@hotmail.it>
2026-06-24 21:50:52 +0200

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,37 @@
+const adminQuali =
+    document.getElementById("admin-quali");
+
+const adminSprintQuali =
+    document.getElementById("admin-sprintquali");
+
+const adminSprint =
+    document.getElementById("admin-sprint");
+
+const adminRace =
+    document.getElementById("admin-race");
+
+function createAdminInputs(container, prefix, total) {
+
+    if (!container) return;
+
+    for (let i = 1; i <= total; i++) {
+
+        let label = `${i}°`;
+
+        if (i === 1) label = "🏆 1°";
+        if (i === 2) label = "🥈 2°";
+        if (i === 3) label = "🥉 3°";
+
+        container.innerHTML += `
+            <div class="result-row">
+                <span>${label}</span>
+                <input id="${prefix}${i}" class="result-field">
+            </div>
+        `;
+    }
+}
+
+createAdminInputs(adminQuali, "adminQr", 5);
+createAdminInputs(adminSprintQuali, "adminSqr", 5);
+createAdminInputs(adminSprint, "adminSr", 8);
+createAdminInputs(adminRace, "adminRr", 10);
