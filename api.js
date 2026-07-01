9a6289e734b05991148d5d59b8490e1b50432bf7
mrco19 <mrco360@hotmail.it>
2026-07-01 15:15:01 +0200

    Update api.js

--- api.js
+++ api.js
@@ -2,23 +2,9 @@
    API FORMULA 1
 ========================================== */
 
-const API_BASE = "https://api.jolpi.ca/ergast/f1";
+async function loadWeekendData() {
 
-async function getCurrentSeason() {
-
-    const year = new Date().getFullYear();
-
-    return year;
+    console.log("Caricamento dati Formula 1...");
 
 }
-async function getNextRace() {
-
-    const season = await getCurrentSeason();
-
-    const response = await fetch(`${API_BASE}/${season}/next.json`);
-
-    const data = await response.json();
-
-    return data;
-
-}
+loadWeekendData();
