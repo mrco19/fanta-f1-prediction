a7535f718574ea211002c55c4fb283e9661132c5
mrco19 <mrco360@hotmail.it>
2026-07-15 20:45:20 +0200

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -5,4 +5,298 @@
    CALENDARIO UFFICIALE 2026
 ========================================================== */
 
-const CALENDAR = [];
+"use strict";
+
+/* ==========================================================
+   FANTA F1 PREDICTION
+   CALENDARIO F1 2026
+========================================================== */
+
+const CALENDAR = [
+
+{
+    round:1,
+    name:"Gran Premio d'Australia",
+    country:"Australia",
+    circuit:"Albert Park",
+    flag:"🇦🇺",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:2,
+    name:"Gran Premio della Cina",
+    country:"Cina",
+    circuit:"Shanghai",
+    flag:"🇨🇳",
+    sprint:true,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:3,
+    name:"Gran Premio del Giappone",
+    country:"Giappone",
+    circuit:"Suzuka",
+    flag:"🇯🇵",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:4,
+    name:"Gran Premio del Bahrain",
+    country:"Bahrain",
+    circuit:"Sakhir",
+    flag:"🇧🇭",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:5,
+    name:"Gran Premio dell'Arabia Saudita",
+    country:"Arabia Saudita",
+    circuit:"Jeddah",
+    flag:"🇸🇦",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:6,
+    name:"Gran Premio di Miami",
+    country:"USA",
+    circuit:"Miami",
+    flag:"🇺🇸",
+    sprint:true,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+   {
+    round:7,
+    name:"Gran Premio dell'Emilia-Romagna",
+    country:"Italia",
+    circuit:"Imola",
+    flag:"🇮🇹",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:8,
+    name:"Gran Premio di Monaco",
+    country:"Monaco",
+    circuit:"Monte Carlo",
+    flag:"🇲🇨",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:9,
+    name:"Gran Premio di Spagna",
+    country:"Spagna",
+    circuit:"Madrid",
+    flag:"🇪🇸",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:10,
+    name:"Gran Premio del Canada",
+    country:"Canada",
+    circuit:"Montréal",
+    flag:"🇨🇦",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:11,
+    name:"Gran Premio d'Austria",
+    country:"Austria",
+    circuit:"Red Bull Ring",
+    flag:"🇦🇹",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:12,
+    name:"Gran Premio di Gran Bretagna",
+    country:"Regno Unito",
+    circuit:"Silverstone",
+    flag:"🇬🇧",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+{
+    round:13,
+    name:"Gran Premio del Belgio",
+    country:"Belgio",
+    circuit:"Spa-Francorchamps",
+    flag:"🇧🇪",
+    sprint:true,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:14,
+    name:"Gran Premio d'Ungheria",
+    country:"Ungheria",
+    circuit:"Hungaroring",
+    flag:"🇭🇺",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:15,
+    name:"Gran Premio dei Paesi Bassi",
+    country:"Paesi Bassi",
+    circuit:"Zandvoort",
+    flag:"🇳🇱",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:16,
+    name:"Gran Premio d'Italia",
+    country:"Italia",
+    circuit:"Monza",
+    flag:"🇮🇹",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:17,
+    name:"Gran Premio dell'Azerbaijan",
+    country:"Azerbaijan",
+    circuit:"Baku City Circuit",
+    flag:"🇦🇿",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:18,
+    name:"Gran Premio di Singapore",
+    country:"Singapore",
+    circuit:"Marina Bay",
+    flag:"🇸🇬",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+   {
+    round:19,
+    name:"Gran Premio degli Stati Uniti",
+    country:"USA",
+    circuit:"Circuit of the Americas",
+    flag:"🇺🇸",
+    sprint:true,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:20,
+    name:"Gran Premio di Città del Messico",
+    country:"Messico",
+    circuit:"Autódromo Hermanos Rodríguez",
+    flag:"🇲🇽",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:21,
+    name:"Gran Premio del Brasile",
+    country:"Brasile",
+    circuit:"Interlagos",
+    flag:"🇧🇷",
+    sprint:true,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:22,
+    name:"Gran Premio di Las Vegas",
+    country:"USA",
+    circuit:"Las Vegas Strip Circuit",
+    flag:"🇺🇸",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:23,
+    name:"Gran Premio del Qatar",
+    country:"Qatar",
+    circuit:"Lusail",
+    flag:"🇶🇦",
+    sprint:true,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+},
+
+{
+    round:24,
+    name:"Gran Premio di Abu Dhabi",
+    country:"Emirati Arabi Uniti",
+    circuit:"Yas Marina",
+    flag:"🇦🇪",
+    sprint:false,
+    startDate:"",
+    endDate:"",
+    predictionsClose:""
+}
+
+];
