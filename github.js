8a021f274ecbb1771e8146df7ef6662144b0d239
mrco19 <mrco360@hotmail.it>
2026-07-16 12:59:41 +0200

    Update github.js

--- github.js
+++ github.js
@@ -4,3 +4,39 @@
    GITHUB API
    FANTA F1 PREDICTION
 ========================================================== */
+"use strict";
+
+/* ==========================================================
+   GITHUB
+========================================================== */
+
+const GITHUB = {
+
+    owner: "mrco19",
+
+    repo: "fanta-f1-prediction",
+
+    branch: "main"
+
+};
+/* ==========================================================
+   TOKEN
+========================================================== */
+
+function getGithubToken(){
+
+    return localStorage.getItem("githubToken") || "";
+
+}
+
+function saveGithubToken(token){
+
+    localStorage.setItem(
+
+        "githubToken",
+
+        token
+
+    );
+
+}
